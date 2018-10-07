a2522f62ec30638bfc84fe3625d56d5e35556de7
Russ Hammett <Kritner@gmail.com>
2018-10-06 20:28:09 -0400

    Initial project creation

---
+++ src/Kritner.OrleansGettingStarted.SiloHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kritner.OrleansGettingStarted.SiloHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

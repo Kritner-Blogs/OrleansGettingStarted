baa88e96131cf1faf11dfbecc410cfdaa1d7b024
Russ Hammett <Kritner@gmail.com>
2018-10-16 23:06:24 -0400

    Stateful grains

---
+++ src/Kritner.OrleansGettingStarted.GrainInterfaces/IVisitTracker.cs
@@ -0,0 +1,11 @@
+﻿using Orleans;
+using System.Threading.Tasks;
+
+namespace Kritner.OrleansGettingStarted.GrainInterfaces
+{
+    public interface IVisitTracker : IGrainWithStringKey
+    {
+        Task<int> GetNumberOfVisits();
+        Task Visit();
+    }
+}
---
+++ src/Kritner.OrleansGettingStarted.Grains/VisitTracker.cs
@@ -0,0 +1,39 @@
+﻿using Kritner.OrleansGettingStarted.GrainInterfaces;
+using Orleans;
+using Orleans.Providers;
+using System;
+using System.Threading.Tasks;
+
+namespace Kritner.OrleansGettingStarted.Grains
+{
+    [StorageProvider(ProviderName = Constants.OrleansMemoryProvider)]
+    public class VisitTracker : Grain<VisitTrackerState>, IVisitTracker
+    {
+        public Task<int> GetNumberOfVisits()
+        {
+            return Task.FromResult(State.NumberOfVisits);
+        }
+
+        public async Task Visit()
+        {
+            var now = DateTime.Now;
+
+            if (!State.FirstVisit.HasValue)
+            {
+                State.FirstVisit = now;
+            }
+
+            State.NumberOfVisits++;
+            State.LastVisit = now;
+
+            await WriteStateAsync();
+        }
+    }
+
+    public class VisitTrackerState
+    {
+        public DateTime? FirstVisit { get; set; }
+        public DateTime? LastVisit { get; set; }
+        public int NumberOfVisits { get; set; }
+    }
+}

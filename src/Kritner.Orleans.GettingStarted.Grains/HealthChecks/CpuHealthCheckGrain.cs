fe75ca442c7358784271997022834a04c0e2f5dc
Szymon Adach <szymon.adach@allegro.pl>
2020-10-15 16:42:56 +0200

    Add more verbose HealthCheck response

--- src/Kritner.Orleans.GettingStarted.Grains/HealthChecks/CpuHealthCheckGrain.cs
+++ src/Kritner.Orleans.GettingStarted.Grains/HealthChecks/CpuHealthCheckGrain.cs
@@ -13,29 +13,33 @@ namespace Kritner.Orleans.GettingStarted.Grains.HealthChecks
 	{
 		private const float UnhealthyThreshold = 90;
 		private const float DegradedThreshold = 70;
-		
+
 		private readonly IHostEnvironmentStatistics _hostEnvironmentStatistics;
 
 		public CpuHealthCheckGrain(IHostEnvironmentStatistics hostEnvironmentStatistics)
 		{
 			_hostEnvironmentStatistics = hostEnvironmentStatistics;
 		}
-		
+
 		public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
 		{
+			if (_hostEnvironmentStatistics.CpuUsage == null)
+			{
+				return Task.FromResult(HealthCheckResult.Unhealthy("Could not determine CPU usage."));
+			}
+
 			if (_hostEnvironmentStatistics.CpuUsage > UnhealthyThreshold)
 			{
 				return Task.FromResult(HealthCheckResult.Unhealthy(
 					$"CPU utilization is unhealthy at {_hostEnvironmentStatistics.CpuUsage}%."));
-				
 			}
-			
+
 			if (_hostEnvironmentStatistics.CpuUsage > DegradedThreshold)
 			{
 				return Task.FromResult(HealthCheckResult.Degraded(
 					$"CPU utilization is degraded at {_hostEnvironmentStatistics.CpuUsage}%."));
 			}
-			
+
 			return Task.FromResult(HealthCheckResult.Healthy(
 				$"CPU utilization is healthy at {_hostEnvironmentStatistics.CpuUsage}%."));
 		}
--- src/Kritner.OrleansGettingStarted.SiloHost/Startup.cs
+++ src/Kritner.OrleansGettingStarted.SiloHost/Startup.cs
@@ -1,9 +1,16 @@
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 using Kritner.OrleansGettingStarted.SiloHost.HealthChecks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 
 namespace Kritner.OrleansGettingStarted.SiloHost
@@ -43,9 +50,56 @@ namespace Kritner.OrleansGettingStarted.SiloHost
 
 			app.UseEndpoints(endpoints =>
 			{
-				endpoints.MapHealthChecks("/health").WithMetadata(new AllowAnonymousAttribute());
+				endpoints.MapHealthChecks(
+						"/health",
+						new HealthCheckOptions
+						{
+							AllowCachingResponses = false,
+							ResponseWriter = WriteResponse
+						})
+					.WithMetadata(new AllowAnonymousAttribute());
 				endpoints.MapControllers();
 			});
+		}
+
+		private static Task WriteResponse(HttpContext context, HealthReport result)
+		{
+			context.Response.ContentType = "application/json; charset=utf-8";
+
+			var options = new JsonWriterOptions
+			{
+				Indented = true
+			};
+
+			using var stream = new MemoryStream();
+			using (var writer = new Utf8JsonWriter(stream, options))
+			{
+				writer.WriteStartObject();
+				writer.WriteString("status", result.Status.ToString());
+				writer.WriteStartObject("results");
+				foreach (var entry in result.Entries)
+				{
+					writer.WriteStartObject(entry.Key);
+					writer.WriteString("status", entry.Value.Status.ToString());
+					writer.WriteString("description", entry.Value.Description);
+					writer.WriteStartObject("data");
+					foreach (var item in entry.Value.Data)
+					{
+						writer.WritePropertyName(item.Key);
+						JsonSerializer.Serialize(
+							writer, item.Value, item.Value?.GetType() ??
+							                    typeof(object));
+					}
+					writer.WriteEndObject();
+					writer.WriteEndObject();
+				}
+				writer.WriteEndObject();
+				writer.WriteEndObject();
+			}
+
+			var json = Encoding.UTF8.GetString(stream.ToArray());
+
+			return context.Response.WriteAsync(json);
 		}
 	}
 }
